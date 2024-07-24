c4e8cc8f897943a4caf478937e4ae7283c91b31e
mominalvi <mominalvi30@gmail.com>
2024-07-24 19:40:12 -0400

    Updated Experience page

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import queens from "./company/queens.png";
+import compsa from "./company/compsa.png";
+import qdaa from "./company/qdaa.png";
+import qmind from "./company/qmind.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  queens,
+  compsa,
+  qdaa,
+  qmind,
   carrent,
   jobit,
   tripguide,
