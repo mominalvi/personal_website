addd9d9d9c857f7e68911cb6ecb27aba98d4a6d0
mominalvi <mominalvi30@gmail.com>
2024-07-26 20:08:17 -0400

    Update contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
+  const formref = useRef()
+  const [from, setForm] = useState ({
+    name: '',
+    email: '',
+    message: '', 
+  })
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {}
+  const handleSubmit = (e) => {}
+
   return (
     <div>Contact</div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "")
